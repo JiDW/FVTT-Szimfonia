b7ea3907b6f5dc4ff1bf6d69d5aa294a35100431
Mathias Latournerie <jidewe@gmail.com>
2020-09-13 19:53:27 +0200

    Nearly done. Only need bumpmaps

---
+++ modules/die.js
@@ -0,0 +1,30 @@
+export class DieSzimfonia extends Die {
+    constructor(termData) {
+        super(termData);
+        this.faces = 6;
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    static DENOMINATION = "s";
+
+    /** @override */
+    get total(){
+        return this.results.length;
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    static getResultLabel(result) {
+        return {
+            "1": '<img src="modules/szimfonia-dice-roller/images/D1_inCHAT.png" />',
+            "2": '<img src="modules/szimfonia-dice-roller/images/F2_inCHAT.png" />',
+            "3": '<img src="modules/szimfonia-dice-roller/images/S1_inCHAT.png" />',
+            "4" : '<img src="modules/szimfonia-dice-roller/images/S2_inCHAT.png" />',
+            "5": '<img src="modules/szimfonia-dice-roller/images/F1_inCHAT.png" />',
+            "6": '<img src="modules/szimfonia-dice-roller/images/D1_inCHAT.png" />'
+        }[result];
+    }
+}
---
+++ modules/szimfonia.js
@@ -0,0 +1,70 @@
+import {DieSzimfonia} from './die.js';
+
+Hooks.once("init", async function () {
+    CONFIG.Dice.terms["s"] = DieSzimfonia;
+});
+
+Hooks.on('diceSoNiceRollComplete', (chatMessageID) => {
+    let message = game.messages.get(chatMessageID);
+    let defense = 0;
+    let focus = 0;
+    let success = 0;
+    let szRoll = false;
+    message.roll.dice.forEach(dice => {
+        if(dice instanceof DieSzimfonia){
+            szRoll = true;
+            dice.results.forEach(res => {
+                switch(res.result){
+                    case 1:
+                        defense++;
+                        break;
+                    case 2:
+                        focus+=2;
+                        break;
+                    case 3:
+                        success++;
+                        break;
+                    case 4:
+                        success+=2;
+                        break;
+                    case 5:
+                        focus++;
+                        break;
+                    case 6:
+                        defense++;
+                        break;
+                }
+            });
+        }
+    });
+    
+    if(szRoll){
+        ChatMessage.create({
+            content: `<b>Defense:</b> ${defense}<br><b>Success:</b> ${success}<br><b>Focus:</b> ${focus}`
+        });
+    }
+});
+
+Hooks.once('diceSoNiceReady', (dice3d) => {
+    dice3d.addSystem({id:"szimfonia",name:"Szimfonia"},true);
+    dice3d.addDicePreset({
+      type:"ds",
+      labels:[
+        'modules/szimfonia-dice-roller/images/D1_bg.png', 
+        'modules/szimfonia-dice-roller/images/F2.png', 
+        'modules/szimfonia-dice-roller/images/S1.png', 
+        'modules/szimfonia-dice-roller/images/S2.png', 
+        'modules/szimfonia-dice-roller/images/F1.png', 
+        'modules/szimfonia-dice-roller/images/D1_bg.png'
+      ],
+      bumpMaps:[
+        'modules/szimfonia-dice-roller/images/D1_bg_bump.png', 
+        'modules/szimfonia-dice-roller/images/F2_bump.png', 
+        'modules/szimfonia-dice-roller/images/S1_bump.png', 
+        'modules/szimfonia-dice-roller/images/S2_bump.png', 
+        'modules/szimfonia-dice-roller/images/F1_bump.png', 
+        'modules/szimfonia-dice-roller/images/D1_bg_bump.png'
+      ],
+      system:"szimfonia"
+    });
+});
