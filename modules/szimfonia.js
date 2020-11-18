f3a13e410963716b5d862ee441d9ad05869b9b72
Mathias Latournerie <jidewe@gmail.com>
2020-11-18 11:34:34 +0100

    Fix message being send by all players instead of author

--- modules/szimfonia.js
+++ modules/szimfonia.js
@@ -6,44 +6,46 @@ Hooks.once("init", async function () {
 
 Hooks.on('diceSoNiceRollComplete', (chatMessageID) => {
     let message = game.messages.get(chatMessageID);
-    let defense = 0;
-    let focus = 0;
-    let success = 0;
-    let szRoll = false;
-    message.roll.dice.forEach(dice => {
-        if(dice instanceof DieSzimfonia){
-            szRoll = true;
-            dice.results.forEach(res => {
-                switch(res.result){
-                    case 1:
-                        defense++;
-                        break;
-                    case 2:
-                        focus+=2;
-                        break;
-                    case 3:
-                        success++;
-                        break;
-                    case 4:
-                        success+=2;
-                        break;
-                    case 5:
-                        focus++;
-                        break;
-                    case 6:
-                        defense++;
-                        break;
-                }
+    if(message.isAuthor){
+        let defense = 0;
+        let focus = 0;
+        let success = 0;
+        let szRoll = false;
+        message.roll.dice.forEach(dice => {
+            if(dice instanceof DieSzimfonia){
+                szRoll = true;
+                dice.results.forEach(res => {
+                    switch(res.result){
+                        case 1:
+                            defense++;
+                            break;
+                        case 2:
+                            focus+=2;
+                            break;
+                        case 3:
+                            success++;
+                            break;
+                        case 4:
+                            success+=2;
+                            break;
+                        case 5:
+                            focus++;
+                            break;
+                        case 6:
+                            defense++;
+                            break;
+                    }
+                });
+            }
+        });
+        
+        if(szRoll){
+            ChatMessage.create({
+                content: `<b>Defense:</b> ${defense}<br><b>Success:</b> ${success}<br><b>Focus:</b> ${focus}`,
+                whisper: message.data.whisper,
+                blind: message.data.blind
             });
         }
-    });
-    
-    if(szRoll){
-        ChatMessage.create({
-            content: `<b>Defense:</b> ${defense}<br><b>Success:</b> ${success}<br><b>Focus:</b> ${focus}`,
-            whisper: message.data.whisper,
-            blind: message.data.blind
-        });
     }
 });
 
